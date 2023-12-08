6c5d80c0f72762f8e8c4a2f3619c42aa5735aba3
Denis Kalkopf <deniskalkopf@gmail.com>
2023-12-08 12:56:27 +0500

    add authorization

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,2 +1,5 @@
 export const ALREADY_REGISTERED_ERROR =
   'Такой пользователь уже был зарегистрирован';
+
+export const USER_NOT_FOUND_ERROR = 'Не верный email или пароль';
+export const WRONG_PASSWORD_ERROR = 'Не верный email или пароль';
