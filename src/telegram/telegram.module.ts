79207eca982821dfac00fb606da7af2fa3d9b9b2
Denis Kalkopf <deniskalkopf@gmail.com>
2023-12-14 11:37:45 +0500

    add forRootAsync telegraf

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,8 +1,32 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Global, Module, Provider } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
+import { ITelegramModuleAsyncOption } from './telegram.interface';
+import { TELEGRAM_MODULE_OPTIONS } from './telegram.constants';
 
-@Module({
-  providers: [TelegramService],
-  exports: [TelegramService],
-})
-export class TelegramModule {}
+@Global()
+@Module({})
+export class TelegramModule {
+  static forRootAsync(options: ITelegramModuleAsyncOption): DynamicModule {
+    const asyncOptions = TelegramModule.createAsyncOptionsProvider(options);
+    return {
+      module: TelegramModule,
+      imports: options.imports,
+      providers: [TelegramService, asyncOptions],
+      exports: [TelegramService],
+    };
+  }
+
+  private static createAsyncOptionsProvider(
+    options: ITelegramModuleAsyncOption,
+  ): Provider {
+    return {
+      provide: TELEGRAM_MODULE_OPTIONS,
+      // tslint:disable-next-line:no-any
+      useFactory: async (...args: any[]) => {
+        const config = await options.useFactory(...args);
+        return config;
+      },
+      inject: options.inject || [],
+    };
+  }
+}
